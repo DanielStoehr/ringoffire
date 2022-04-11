75b3021f5fc957ed232021007c1bbdbca5b82524
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-04-11 15:06:10 +0200

    add animation

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {Game} from "../../models/game";
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
+  currentCard = ''
+
+  constructor() {
+    this.game = new Game()
+  }
+
+  ngOnInit(): void {
+    this.newGame()
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game)
+  }
+
+  takeCard() {
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!
+      console.log(this.currentCard)
+      this.pickCardAnimation = true
+
+      setTimeout(()=>{
+        this.pickCardAnimation = false
+      },1500)
+    }
+
+  }
+
+}
